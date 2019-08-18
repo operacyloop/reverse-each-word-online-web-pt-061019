824d870bfbbb237d1aa5f335911722cce8425401
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-08-18 17:59:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,14 @@
-def reverse_each_word(string) # takes in a string
-new_array = [] # creates a new array to put the reversed words
-string = string.split(" ") #splits the string up into pieces 
-i = 0 #sets counter to zero
-  while i < string.length # go from 0 to end of string
-    new_array << string[i].reverse # add reversed words to new array
-    i += 1 #add one to counter
-  end 
-  string = string.collect
-return new_array.join(" ") #return the string with the words reversed
-end
+# def reverse_each_word(string) # takes in a string
+# new_array = [] # creates a new array to put the reversed words
+# string = string.split(" ") #splits the string up into pieces 
+# i = 0 #sets counter to zero
+#   while i < string.length # go from 0 to end of string
+#     new_array << string[i].reverse # add reversed words to new array
+#     i += 1 #add one to counter
+#   end 
+#   string = string.collect
+# return new_array.join(" ") #return the string with the words reversed
+# end
 
 def reverse_each_word(string)
 string = string.split(" ")
